dccc4a28c98808d9139c9cf31eb5b2c0613d6899
Valentin Dauvier <valentindauvier@hotmail.fr>
2022-03-24 19:06:40 +0100

    réalisations des sections a propos, blog, destination, galleries, services et témoignages

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,8 +1,19 @@
+// Menu hamburger
+
 let navbar = document.querySelector('#header #navbar');
 
 let boutonMenu = document.getElementById('menu-btn');
 
 boutonMenu.addEventListener('click', function(e) {
     navbar.classList.toggle('active');
+})
+
+// Change vidéo "apropos"
+
+document.querySelectorAll('.apropos .apropos__video-container .apropos__controls .apropos__control-btn').forEach(btn =>{
+    btn.onclick = () => {
+        let src = btn.getAttribute('data-src');
+        document.querySelector('.apropos .apropos__video-container .apropos__video').src = src;
+    }
 })
 
